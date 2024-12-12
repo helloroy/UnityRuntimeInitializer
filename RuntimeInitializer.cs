1df547b0b66d7f934b68e6ea20d5bb1ef9559ba8
Roy Ng <helloroy@gmail.com>
2024-12-12 18:32:45 +0800

    Update RuntimeInitializer.cs

--- RuntimeInitializer.cs
+++ RuntimeInitializer.cs
@@ -5,7 +5,7 @@ using System.IO;
 [HelpURL("https://docs.unity3d.com/ScriptReference/RuntimeInitializeOnLoadMethodAttribute.html")]
 public class RuntimeInitializer : ScriptableObject
 {
-    private static string fileName = "RuntimeInitializerSetting"; // Change this if needed, but remember to rename the exists asset as well.
+    private static string fileName = "RuntimeInitializerSetting"; // Change this if needed, but remember to rename the existing asset as well.
     private static RuntimeInitializer _instance;
     private static RuntimeInitializer Instance
     {
@@ -20,54 +20,54 @@ public class RuntimeInitializer : ScriptableObject
     }
     [SerializeField] private bool debug;
     [Tooltip("When starting up the runtime. Called before the first scene is loaded.")]
-    [SerializeField] private GameObject[] subsystemRegistration;
+    [SerializeField] private GameObject[] subsystemRegistrationPrefabs;
     [Tooltip("Before the splash screen is shown. At this time the objects of the first scene have not been loaded yet.")]
-    [SerializeField] private GameObject[] beforeSplashScreen;
+    [SerializeField] private GameObject[] beforeSplashScreenPrefabs;
     [Tooltip("When all assemblies are loaded and preloaded assets are initialized. At this time the objects of the first scene have not been loaded yet.")]
-    [SerializeField] private GameObject[] afterAssembliesLoaded;
+    [SerializeField] private GameObject[] afterAssembliesLoadedPrefabs;
     [Tooltip("When the first scene's objects are loaded into memory but before Awake has been called.")]
-    [SerializeField] private GameObject[] beforeSceneLoad;
+    [SerializeField] private GameObject[] beforeSceneLoadPrefabs;
     [Tooltip("When the first scene's objects are loaded into memory and after Awake has been called.")]
-    [SerializeField] private GameObject[] afterSceneLoad;
+    [SerializeField] private GameObject[] afterSceneLoadPrefabs;
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
     static void SubsystemRegistration()
     {
-        if (Instance && Instance.debug == true) { Debug.Log($"{nameof(RuntimeInitializer)}: Start instantiation by <b>{fileName}</b>", Instance); }
-        if (Instance) InstantiateAll(Instance.subsystemRegistration, RuntimeInitializeLoadType.SubsystemRegistration);
+        if (Instance && Instance.debug) { Debug.Log($"{nameof(RuntimeInitializer)}: Start instantiation by <b>{fileName}</b>", Instance); }
+        if (Instance) InstantiateAll(Instance.subsystemRegistrationPrefabs, RuntimeInitializeLoadType.SubsystemRegistration);
     }
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
     static void BeforeSplashScreen()
     {
-        if (Instance) InstantiateAll(Instance.beforeSplashScreen, RuntimeInitializeLoadType.BeforeSplashScreen);
+        if (Instance) InstantiateAll(Instance.beforeSplashScreenPrefabs, RuntimeInitializeLoadType.BeforeSplashScreen);
     }
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
     static void AfterAssembliesLoaded()
     {
-        if (Instance) InstantiateAll(Instance.afterAssembliesLoaded, RuntimeInitializeLoadType.AfterAssembliesLoaded);
+        if (Instance) InstantiateAll(Instance.afterAssembliesLoadedPrefabs, RuntimeInitializeLoadType.AfterAssembliesLoaded);
     }
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     static void BeforeSceneLoad()
     {
-        if (Instance) InstantiateAll(Instance.beforeSceneLoad, RuntimeInitializeLoadType.BeforeSceneLoad);
+        if (Instance) InstantiateAll(Instance.beforeSceneLoadPrefabs, RuntimeInitializeLoadType.BeforeSceneLoad);
     }
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
     static void AfterSceneLoad()
     {
-        if (Instance) InstantiateAll(Instance.afterSceneLoad, RuntimeInitializeLoadType.AfterSceneLoad);
+        if (Instance) InstantiateAll(Instance.afterSceneLoadPrefabs, RuntimeInitializeLoadType.AfterSceneLoad);
     }
 
-    static void InstantiateAll(GameObject[] objectList, RuntimeInitializeLoadType type)
+    static void InstantiateAll(GameObject[] prefabList, RuntimeInitializeLoadType type)
     {
-        foreach (GameObject prefab in objectList)
+        foreach (GameObject prefab in prefabList)
         {
-            GameObject addedPrefab = Instantiate(prefab);
-            addedPrefab.name = prefab.name;
-            if (Instance.debug) Debug.Log($"{nameof(RuntimeInitializer)}: Instantiate <b>{addedPrefab.name}</b> on {type}", prefab);
+            GameObject instantiatedPrefab = Instantiate(prefab);
+            instantiatedPrefab.name = prefab.name;
+            if (Instance.debug) Debug.Log($"{nameof(RuntimeInitializer)}: Instantiate <b>{instantiatedPrefab.name}</b> on {type}", prefab);
         }
     }
 
@@ -84,7 +84,7 @@ public class RuntimeInitializer : ScriptableObject
 
         if (asset)
         {
-            Debug.LogWarning($"{nameof(RuntimeInitializer)}: <b>{fileName}</b> asset exists already, you may rename the exists one.", asset);
+            Debug.LogWarning($"{nameof(RuntimeInitializer)}: <b>{fileName}</b> asset already exists, you may rename the existing one.", asset);
         }
         else
         {
